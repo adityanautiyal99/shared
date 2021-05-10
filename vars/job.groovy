4ee452bab04814ef7bb1f2ea74c50c61d7e40ed7
Aditya Nautiyal <77630833+adityanautiyal99@users.noreply.github.com>
2021-05-10 13:53:01 +0530

    Update job.groovy

--- vars/job.groovy
+++ vars/job.groovy
@@ -5,7 +5,7 @@ def call(String repoUrl) {
                    sh "java -version"
                }
            stage("Checkout Code") {
-                   git url: "${repoUrl}"
+                   git "${repoUrl}"
                }
        }
 }
