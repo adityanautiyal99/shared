9af6bdee2dc55bb124915af237be92ae43be1849
Aditya Nautiyal <77630833+adityanautiyal99@users.noreply.github.com>
2021-05-18 13:13:33 +0530

    Update job.groovy

--- vars/job.groovy
+++ vars/job.groovy
@@ -1,11 +1,6 @@
 def call(String repoUrl) {
-  node {
-           stage("Tools initialization") {
-                   sh "mvn --version"
-                   sh "java -version"
-               }
-           stage("cloning repo") {
-                   git "${repoUrl}"
-               }
-       }
-}
+  sh '''
+  mvn --version
+  java -version
+  '''
+}          
